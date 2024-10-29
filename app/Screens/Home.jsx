79c17314408a40e01595faab4544b698dd9bec66
emnuelht <wellingtonemanuel510@gmail.com>
2024-10-28 22:48:33 -0300

    Na Home.jsx eu adicionei um campo para alterar a info de confirmação, pois se nao fizer isso, toda vez no login ele vai pedir confirmação novamente, pois ele acha que ainda nfalta concluir essa etapa, que é de confirmação do código.

--- app/Screens/Home.jsx
+++ app/Screens/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import {View, Text, TouchableOpacity, SafeAreaView, ScrollView, StatusBar, Image} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity, SafeAreaView, ScrollView, StatusBar, Image, Alert} from 'react-native';
 import Icon from "react-native-vector-icons/MaterialIcons";
 import CommandStyles from "../Styles/CommandStyles";
+import {Network} from "../config/Network";
+import {CustomAlertInternet} from "./Items";
+import Async from "../config/Async";
 
-const commandStyle = CommandStyles;
+
+const checkConnection = async () => {
+    return await Network.isConnected();
+};
+
 const Toolbar = ({ navigation }) => {
     return (
         <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', backgroundColor: '#00a3ff', padding: 10}}>
@@ -17,6 +24,33 @@ const Toolbar = ({ navigation }) => {
 }
 
 export default function Home({ navigation }) {
+    const [modalVisible, setModalVisible] = useState(false);
+
+    checkConnection().then(result => {
+        if (result) {
+            new Async().getToken('login-email').then((token) => {
+                new Network().dataUsuario(token).then((result) => {
+                    if (result.success) {
+                        if (result.data.confirm_code === 0) {
+                            new Network().setConfirmCode(token).then();
+                        }
+                    } else {
+                        Alert.alert('Ops algo deu errado! Por favor tente novamente.',
+                            '',
+                            [
+                                {
+                                    text: 'Tentar novamente',
+                                    onPress: () => {navigation.replace('Home');},
+                                }
+                            ],
+                            {cancelable: false},);
+                    }
+                })
+            })
+        } else {
+            setModalVisible(true);
+        }
+    })
     return (
         <View style={{backgroundColor: '#fff', flex: 1}}>
             <Toolbar navigation={navigation} />
@@ -25,7 +59,7 @@ export default function Home({ navigation }) {
                 <ScrollView>
                     <StatusBar backgroundColor={'#000'} barStyle="light-content" />
 
-                    <View style={{backgroundColor: '#00a3ff', paddingHorizontal: 20, paddingTop: 50, paddingBottom: 50, borderBottomStartRadius: 100, borderBottomEndRadius: 100, borderBottomColor: '#000', marginTop: -5,
+                    <View style={{backgroundColor: '#00a3ff', paddingHorizontal: 20, paddingTop: 50, paddingBottom: 50, borderBottomStartRadius: 100, borderBottomEndRadius: 100, borderBottomColor: '#000', marginTop: -10,
                         // Shadow para iOS
                         shadowColor: '#000',
                         shadowOffset: { width: 0, height: 5 },
@@ -34,8 +68,8 @@ export default function Home({ navigation }) {
 
                         // Sombra para Android
                         elevation: 5,}}>
-                        <View style={commandStyle.container_logo}>
-                            <Image source={require("../../assets/images/logo_app.png")} style={commandStyle.logo_app} />
+                        <View style={CommandStyles.container_logo}>
+                            <Image source={require("../../assets/images/logo_app.png")} style={CommandStyles.logo_app} />
                             <Text style={{ fontWeight: 'bold' }}>Services<Text style={{ color: '#fff' }}>Here</Text></Text>
                         </View>
 
@@ -48,8 +82,8 @@ export default function Home({ navigation }) {
                         <Icon name='work' size={30} style={{textAlign: 'center', marginTop: 50, marginBottom: 20}}/>
                         <Text style={{textAlign: 'center', fontSize: 18, fontWeight: 'bold'}}>Está procurando trabalho?</Text>
                         <Text style={{textAlign: 'center', fontSize: 18, marginBottom: 20}}>Seja um emprego fixo ou temporário, aqui você encontra diversas oportunidades. Navegue e descubra a vaga ideal para você!</Text>
-                        <TouchableOpacity style={commandStyle.button} onPress={() => navigation.navigate('Services')}>
-                            <Text style={commandStyle.buttonText}>PROCURAR SERVIÇOS</Text>
+                        <TouchableOpacity style={CommandStyles.button} onPress={() => navigation.navigate('Services')}>
+                            <Text style={CommandStyles.buttonText}>PROCURAR SERVIÇOS</Text>
                         </TouchableOpacity>
                     </View>
 
@@ -57,10 +91,11 @@ export default function Home({ navigation }) {
                         <Icon name='person' size={30} style={{textAlign: 'center', marginTop: 50, marginBottom: 20}}/>
                         <Text style={{textAlign: 'center', fontSize: 18, fontWeight: 'bold'}}>Precisa de um profissional?</Text>
                         <Text style={{textAlign: 'center', fontSize: 18, marginBottom: 20}}>Encontre trabalhadores qualificados para serviços temporários ou fixos. A solução ideal para sua demanda está a um clique de distância!</Text>
-                        <TouchableOpacity style={commandStyle.button} onPress={() => navigation.navigate('Professional')}>
-                            <Text style={commandStyle.buttonText}>PROCURAR PROFISSIONAIS</Text>
+                        <TouchableOpacity style={CommandStyles.button} onPress={() => navigation.navigate('Professionals')}>
+                            <Text style={CommandStyles.buttonText}>PROCURAR PROFISSIONAIS</Text>
                         </TouchableOpacity>
                     </View>
+                    <CustomAlertInternet icon={'wifi-off'} color={'#000000'} title={'Sem Internet'} message={'Por favor, verifique sua conexão e tente novamente!\nSe o erro persistir tente sair e entrar novamente.'} setModalVisible={setModalVisible} modalVisible={modalVisible} />
                 </ScrollView>
             </SafeAreaView>
         </View>
