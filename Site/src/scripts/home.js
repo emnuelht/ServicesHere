8737286f65fafd9c8a5dfd5b11fc3b57bf7a4a05
emnuelht <wellingtonemanuel510@gmail.com>
2024-11-04 05:27:27 -0300

    Criando o site

---
+++ Site/src/scripts/home.js
@@ -0,0 +1,3 @@
+const pagAtual = document.querySelector('.container_menu_lateral__list_menu li:nth-child(1)');
+
+pagAtual.style.color = '#00a3ff';
---
+++ Site/src/scripts/sobre_app.js
@@ -0,0 +1,33 @@
+const clickSobre = document.querySelector('.container_menu_lateral__list_menu li:nth-child(2)');
+let click = true;
+
+clickSobre.addEventListener('click', () => {
+    const seta = clickSobre.querySelector('svg');
+    const containerListaOculta = document.querySelector('.container_lists_ocultas');
+    const listaOculta = document.querySelector('.list_menu__list_sobre');
+     if (click) {
+         seta.style.transform = 'rotate(90deg)';
+         clickSobre.style.color = '#00a3ff';
+         seta.style.fill = '#00a3ff';
+
+         listaOculta.style.display = 'block';
+         setTimeout(() => {
+             containerListaOculta.style.height = '70%'
+             listaOculta.style.transform = 'translateY(0)';
+         }, 10);
+
+         click = false;
+     } else {
+         clickSobre.style.color = '#000';
+         seta.style.fill = '#000';
+         seta.style.transform = 'rotate(0deg)';
+
+         listaOculta.style.transform = 'translateY(-100%)';
+         setTimeout(() => {
+             containerListaOculta.style.height = '0';
+             setTimeout(() => listaOculta.style.display = 'none', 100);
+         }, 200);
+
+         click = true;
+     }
+});
